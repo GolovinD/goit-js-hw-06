7e684d79cc6119e3282c10fc352669da54eb6a42
GolovinD <golovin.dmytro@gmail.com>
2022-10-02 17:47:47 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,10 +5,11 @@ formRef.addEventListener('submit', (event) => {
     if (event.currentTarget.email.value === ''
         || event.currentTarget.password.value === '') {
         alert`Всі поля повинні бути заповнені!`
-    }
-    const email = formRef.email;
-    const password = formRef.password;
-    const formData = { email, password }
+    };
+    const email = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value;
+    const formData = {email, password}
+
     console.log(formData)
 
     event.currentTarget.reset();
